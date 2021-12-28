d790c4a2b3148e71f4b271187a4accb6701f1be0
Sharp0802 <sharp0802.business@gmail.com>
2021-12-28 23:59:27 +0900

    Refactoring

---
+++ ParallelTCP/Shared/MessageChannel.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Concurrent;
+using ParallelTCP.Common;
+
+namespace ParallelTCP.Shared;
+
+public class MessageChannel : IDisposable
+{
+    public MessageChannel(Guid id)
+    {
+        StreamId = id;
+    }
+
+    private Guid StreamId { get; }
+
+    private ConcurrentQueue<SharedMessage> MessageQueue { get; } = new();
+
+    public event SharedMessageEventHandler? MessageReceived;
+    public event SharedMessageEventHandler? MessageTransmitted;
+    public event CommonEventHandler? Disposing;
+
+    internal Task OnTransmitted(SharedMessage msg)
+    {
+        return MessageTransmitted.InvokeAsync(this, new SharedMessageEventArgs(StreamId, msg));
+    }
+
+    internal Task OnReceived(SharedMessage msg)
+    {
+        return MessageReceived.InvokeAsync(this, new SharedMessageEventArgs(StreamId, msg));
+    }
+
+    public Task<SharedMessage?> TransmitToReceive(SharedMessage msg)
+    {
+        var result = default(SharedMessage);
+        var waiter = new AutoResetEvent(false);
+
+        Task Handler(object? sender, SharedMessageEventArgs args)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                try
+                {
+                    if (!args.SharedMessage.Header.ReplyTo.Equals(msg.Header.MessageId)) return;
+                    result = args.SharedMessage;
+                    waiter.Set();
+                    waiter.Close();
+                    waiter.Dispose();
+                    MessageReceived -= Handler;
+                }
+                catch (Exception)
+                {
+#if DEBUG
+                    throw;
+#endif
+                }
+            });
+        }
+
+        MessageReceived += Handler;
+        return QueueMessage(msg).ContinueWith(_ =>
+        {
+            if (!waiter.WaitOne())
+                throw new InvalidOperationException("failed to wait one via AutoResetEvent");
+            return result;
+        });
+    }
+
+    public Task QueueMessage(SharedMessage msg)
+    {
+        var waiter = new AutoResetEvent(false);
+
+        Task TransmitChecker(object? sender, SharedMessageEventArgs args)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                if (!args.SharedMessage.Header.MessageId.Equals(msg.Header.MessageId)) return;
+                try
+                {
+                    waiter.Set();
+                    waiter.Close();
+                    waiter.Dispose();
+                    MessageTransmitted -= TransmitChecker;
+                }
+                catch (Exception)
+                {
+#if DEBUG
+                    throw;
+#endif
+                }
+            });
+        }
+
+        MessageTransmitted += TransmitChecker;
+        return Task.Factory.StartNew(() =>
+        {
+            MessageQueue.Enqueue(msg);
+            if (!waiter.WaitOne())
+                throw new InvalidOperationException("failed to wait one via AutoResetEvent");
+        });
+    }
+
+    public void Dispose()
+    {
+        Disposing.InvokeAsync(this, new CommonEventArgs()).Wait();
+    }
+}
---
+++ ParallelTCP/Shared/NetworkMessageEventHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace ParallelTCP.Shared;
+
+public delegate Task NetworkMessageEventHandler(object? sender, NetworkMessageEventArgs args);
+
+public class NetworkMessageEventArgs : EventArgs
+{
+    public NetworkMessageEventArgs(NetworkMessage message)
+    {
+        Message = message;
+    }
+
+    public NetworkMessage Message { get; }
+}
