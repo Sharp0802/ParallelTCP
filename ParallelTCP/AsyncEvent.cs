809d48e0984a99743a71789dc24f45ceb1cd0055
Sharp0802 <sharp0802.business@gmail.com>
2021-12-28 20:43:53 +0900

    Add extension for async event

---
+++ ParallelTCP/AsyncEvent.cs
@@ -0,0 +1,11 @@
+﻿namespace ParallelTCP;
+
+public static class AsyncEvent
+{
+    public static Task InvokeAsync<TEventArgs>(this Delegate? @event, object? sender, TEventArgs args)
+    {
+        return @event is null 
+            ? Task.CompletedTask 
+            : Task.WhenAll(@event.GetInvocationList().Select(d => (Task) d.DynamicInvoke(sender, args)));
+    }
+}
