012dc849ab484779221dcf1cb49ba671be44e292
Sharp0802 <sharp0802.business@gmail.com>
2021-12-30 23:11:37 +0900

    In progress

---
+++ ParallelTCP/ServerSide/Server.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Concurrent;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ParallelTCP.ServerSide;
+
+public class Server
+{
+    public Server(int port)
+    {
+        LocalEndpoint = new IPEndPoint(IPAddress.Any, port);
+    }
+
+    private IPEndPoint LocalEndpoint { get; }
+    
+    private TcpListener? TcpListener { get; set; }
+    private ConcurrentDictionary<Guid, TcpClient> Clients { get; } = new();
+
+    public Task OpenAsync()
+    {
+        TcpListener = new TcpListener(LocalEndpoint);
+        return Task.CompletedTask;
+    }
+}
