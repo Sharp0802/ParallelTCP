fc4f57f6ee8879f9555f1dd511c64069ae43859d
Sharp0802 <sharp0802.business@gmail.com>
2022-01-01 17:53:16 +0900

    Impl example server

---
+++ ParallelTCP.Test.Server/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using ParallelTCP.Shared;
+using ParallelTCP.Shared.Handlers;
+using ParallelTCP.Shared.Messages;
+
+namespace ParallelTCP.Test.Server;
+
+internal class Program
+{
+    private async Task Main()
+    {
+        Console.Write("write the port to communicate:");
+        if (!int.TryParse(Console.ReadLine() ?? string.Empty, out var port))
+        {
+            Console.WriteLine("error: cannot parse port number.");
+            return;
+        }
+        await using var server = new ServerSide.Server(port);
+        server.ClientConnected += ServerOnClientConnected;
+        await server.OpenAsync();
+        await server.RunAsync();
+    }
+
+    private List<(MessageContext Context, MessageChannel Channel)> Clients { get; } = new();
+
+    private async Task ServerOnClientConnected(object? sender, NetworkConnectionEventArgs args)
+    {
+        if (args.Context is null) return;
+        var channel = await args.Context.GetChannelAsync(Guid.Empty);
+        Clients.Add((args.Context, channel));
+        channel.MessageReceived += ChannelOnMessageReceived;
+    }
+
+    private async Task ChannelOnMessageReceived(object? sender, SharedMessageEventArgs args)
+    {
+        foreach (var (context, channel) in Clients)
+        {
+            try
+            {
+                await channel.SendAsync(new SharedMessage(Guid.Empty, args.SharedMessage.Content));
+            }
+            catch (IOException)
+            {
+                Clients.Remove((context, channel));
+                await context.DisconnectAsync();
+            }
+        }
+    }
+}
