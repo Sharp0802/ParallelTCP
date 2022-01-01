3a0d8d13b9d9223fbdc98cd761e35a9551c19a07
Sharp0802 <sharp0802.business@gmail.com>
2022-01-01 16:57:46 +0900

    Add comments

--- ParallelTCP/Shared/IMessage.cs
+++ ParallelTCP/Shared/IMessage.cs
@@ -2,7 +2,14 @@
 
 public interface IMessage
 {
+    /// <summary>
+    /// Content of this message
+    /// </summary>
     public byte[] Content { get; }
     
+    /// <summary>
+    /// Serialize this message into a byte array
+    /// </summary>
+    /// <returns>the serialization result of this message</returns>
     public byte[] ToBytes();
 }
