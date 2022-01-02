7ad35f7f14b103ef4b8cf88a8940bcb9bfca475f
Sharp0802 <sharp0802.business@gmail.com>
2022-01-02 18:06:57 +0900

    Fix auto versioning

--- ParallelTCP/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Resources;
-
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
-
